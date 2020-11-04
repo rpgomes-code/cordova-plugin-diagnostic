db74cdb5753f1e915dc2aac8c88288ee38b27f76
OutsystemsNOS <45230356+OutsystemsNOS@users.noreply.github.com>
2020-11-04 16:11:22 +0000

    Update Diagnostic_External_Storage.java

--- src/android/Diagnostic_External_Storage.java
+++ src/android/Diagnostic_External_Storage.java
@@ -25,7 +25,8 @@ package cordova.plugins;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
-import androidx.core.os.EnvironmentCompat;
+//import androidx.core.os.EnvironmentCompat;
+import android.support.v4.os.EnvironmentCompat;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
